472830fcb73ecd6748216649437ef613df8169d4
kezeahdongon <kezeahdongon54@gmail.com>
2021-10-07 21:59:42 +0800

    fourth commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const login = document.querySelector('#log-in');
+const loginPage = document.querySelector('#log_in');
+
+// main page log-in button
+login.addEventListener('click', e => {
+  e.preventDefault();
+  location.replace('log-in.html');
+});
+
+// log-in page log-in button
+loginPage.addEventListener('click', e  => {
+  e.preventDefault();
+  location.replace('chat-area.html');
+});
